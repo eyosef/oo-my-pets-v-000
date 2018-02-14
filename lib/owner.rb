19995512c739d24ad9e24ed6b9d1c3f81e3a8705
Eden Mesfun <emesfun@gmail.com>
2018-02-14 17:54:09 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -85,10 +85,10 @@ attr_reader :species
     dog_counter = @pets[:dogs].count
     cat_counter = @pets[:cats].count
     fish_counter = @pets[:fishes].count
-    binding.pry
+    #binding.pry
 
 
-    @pets.each_with_index { |key, value| puts "I have #{key} #{value}"}
+    #@pets.each_with_index { |key, value| puts "I have #{key} #{value}"}
   end
 
 
