0046edab2175c9a88a933dbc6237c3cbb655e42a
Eden Mesfun <emesfun@gmail.com>
2018-02-14 14:44:06 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,62 @@
+require 'pry'
+
 class Owner
-  # code goes here
+
+attr_accessor :name, :pets
+
+attr_reader :species
+
+@@all = []
+@@count = 0
+
+  def initialize(name)
+    @name = name
+    @@all << self
+    @@count += 1
+    @pets = {:fishes => [], :dogs => [], :cats => []}
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.reset_all
+    @@count = 0
+  end
+
+  def self.count
+    @@count
+  end
+
+  def species
+    @species = self.name
+  end
+
+  def say_species
+    "I am a #{self.name}."
+  end
+
+  def pets
+    @pets
+  end
+
+  def buy_fish(fish_name)
+    fish_name = Fish.new(fish_name)
+    @pets[:fishes] << fish_name
+  end #method
+
+  def buy_cat(cat_name)
+    cat_name = Cat.new(cat_name)
+    @pets[:cats] << cat_name
+  end
+
+  def buy_dog(dog_name)
+    dog_name = Dog.new(dog_name)
+    @pets[:dogs] << dog_name
+  end
+
+  def walk_dogs
+    
+  end
+
 end
